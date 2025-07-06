5b0ad04f03edf35ea5cdd181be83d036e5e36055
pepega <werewrs555@gmail.com>
2025-07-06 13:46:10 +0300

    refactor(web: Card page) : Made more information displayed

--- src/main/resources/static/style/card-order-page-style/card-info-expand.js
+++ src/main/resources/static/style/card-order-page-style/card-info-expand.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll(".card-button");
     const infoText = document.querySelector(".card-info");
+    const lawInfo = document.querySelector(".card-law")
+    const cashbackInfo = document.querySelector(".card-cashback")
     const orderLink = document.querySelector(".apply-for-a-card-container");
     const cardNameSpan = document.querySelector(".card-name");
 
@@ -8,10 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
         button.addEventListener("click", () => {
             const name = button.dataset.name;
             const info = button.dataset.info;
+            const law = button.dataset.law;
+            const cashback = parseFloat(button.dataset.cashback);
             const link = button.dataset.link;
 
             cardNameSpan.textContent = name;
             infoText.textContent = info;
+            lawInfo.textContent = law + "₽";
+            cashbackInfo.textContent = cashback.toFixed(1)+ "%";
             orderLink.setAttribute("href", link);
         });
     });
