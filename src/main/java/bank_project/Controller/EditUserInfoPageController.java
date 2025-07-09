a9d31692b5d6677d32dbdbf8c93a809f3d4ee128
pepega <werewrs555@gmail.com>
2025-07-09 11:42:55 +0300

    fix(edit user info) : Fixed minor bug
    A minor bug that after changing information immediately redirected to the user's page and interfered with the application's operation has been fixed

--- src/main/java/bank_project/Controller/EditUserInfoPageController.java
+++ src/main/java/bank_project/Controller/EditUserInfoPageController.java
@@ -35,6 +35,6 @@ public class EditUserInfoPageController {
         }catch (Exception e) {
             model.addAttribute("error", e.getMessage());
         }
-        return "user-page";
+        return "edit-user-info-page";
     }
 }
