7ef36ed316ef84f5349220c5a42260fc9e1fb218
pepega <werewrs555@gmail.com>
2025-07-10 14:47:30 +0300

    feat(migration) : Added V5 db migration

---
+++ src/main/resources/db/migration/V5_fill_fields_in_table_goal_templates.sql
@@ -0,0 +1,4 @@
+INSERT INTO goal_templates (goal_name) VALUES ('Saving account'),
+                                              ('Deposit'),
+                                              ('Deposit account'),
+                                              ('Budget account')
